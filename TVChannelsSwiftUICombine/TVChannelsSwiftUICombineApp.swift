dbaba6413e62216872006d085915df976ee254d0
Imariel <mail@fdmi3.ru>
2022-01-17 17:18:09 +0400

    Init

---
+++ TVChannelsSwiftUICombine/TVChannelsSwiftUICombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  TVChannelsSwiftUICombineApp.swift
+//  TVChannelsSwiftUICombine
+//
+//  Created by Дмитрий Фёдоров on 17.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct TVChannelsSwiftUICombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
